46f5929cbb5fcffbfc67878a90d07dc27aebb575
Kevin Canlas <kvncnlsdsgns@gmail.com>
2020-12-10 21:46:53 -0500

    Added functionality to the frequency button.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* BUTTON TOGGLE */
 const toggleBtn = document.querySelector(".header__frequency__toggle-btn");
 
 let isBtnInAction = false;
@@ -5,15 +6,17 @@ toggleBtn.addEventListener("click", () => {
   if (isBtnInAction) {
     reverseAnimation();
     isBtnInAction = false;
+    monthlyPrices();
     return false;
   }
   isBtnInAction = true;
+  annualPrices();
   startAnimation();
 });
 
 let startAnimation = () => {
   gsap.to(".circle", {
-    x: 24,
+    x: -24,
     duration: 0.25,
     ease: "power1",
   });
@@ -26,3 +29,23 @@ let reverseAnimation = () => {
     ease: "power1",
   });
 };
+
+const basicPrice = document.querySelector(".basic-price p");
+const proPrice = document.querySelector(".pro-price p");
+const masterPrice = document.querySelector(".master-price p");
+
+console.log(basicPrice);
+console.log(proPrice);
+console.log(masterPrice);
+
+function monthlyPrices() {
+  basicPrice.textContent = "19.99";
+  proPrice.textContent = "24.99";
+  masterPrice.textContent = "39.99";
+}
+
+function annualPrices() {
+  basicPrice.textContent = "199.99";
+  proPrice.textContent = "249.99";
+  masterPrice.textContent = "399.99";
+}
